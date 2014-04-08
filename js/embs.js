96ed62b1a16b535cdc55494d88c16825cab39897
Jeremy <c.yongfang@gmail.com>
2014-04-08 14:02:55 +0800

    change the router of angularjs's default route with angularjs-ui-router for nesting views

--- js/embs.js
+++ js/embs.js
@@ -4,52 +4,32 @@
  * date: 2014.04.16
  * embs application definition
  * Version: 1.0
- * License: MIT
+ * License: siyaa inc
  */
- var EMBS = angular.module('embs',['ngRoute']);
 
-//inject service provider (routeProvider)
- EMBS.config(['$routeProvider',function($routeProvider){
- 	// register user route
- 	$routeProvider.when(
- 		'/user/login',
+ var EMBS = angular.module('embs',['ui.router']);
+
+//inject service provider($stateProvider,$urlRouteProvider) into our application
+
+ EMBS.config(function($stateProvider,$urlRouterProvider){
+
+ 	// For any unmatched url, redirect to /notfound
+ 	$urlRouterProvider.otherwise('/notfound');
+
+ 	//register user route
+ 	$stateProvider.state(
+ 		'user',
  		{
+ 			url:'/user',
+ 			templateUrl:'views/user.html'
+ 		}
+ 	)
+ 	.state(
+ 		'user.login',
+ 		{
+ 			url:'/login',
  			templateUrl:'views/partials/u-login.html'
- 			resolve:
- 			{
- 				//register controller dependencies whitch like css style...
- 				style:function(){
-
- 				}
- 			}
- 		}
- 	)
- 	.when(
- 		'/user/register',
- 		{
- 			templateUrl:'views/partials/u-register.html'
- 		}
- 	)
-
-
- 	//register chtone route
- 	.when(
- 		'chtone/login',
- 		{
- 			templateUrl:'views/partials/ct-login.html'
- 		}
- 	)
- 	.when(
- 		'chtone/register',
- 		{
- 			templateUrl:'views/partials/ct-register.html'
- 		}
- 	)
-
- 	//register other route
- 	.otherwise(
- 		{
- 			redirectTo:'notfound.html'
  		}
  	);
- }]);
+ 	
+ });
