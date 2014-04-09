1cb4e1c827534bab82a7ff635c2cdd6cc7c9a548
Jeremy <c.yongfang@gmail.com>
2014-04-09 13:36:21 +0800

    add some denpendencies injection when state change and add layout template's navigation

--- js/embs.js
+++ js/embs.js
@@ -21,7 +21,8 @@
  		'user',
  		{
  			url:'/user',
- 			templateUrl:'views/user.html'
+ 			templateUrl:'views/user.html',
+ 			css:'/css/user.css'
  		}
  	)
  	.state(
@@ -33,3 +34,36 @@
  	);
  	
  });
+
+ //compile the head with extra things such like stylesheet、title etc. 
+ EMBS.directive('head', ['$rootScope','$compile',
+ 	function($rootScope, $compile){
+ 		return {
+ 			restrict: 'E',
+ 			link: function(scope, elem){
+ 				var html = '<link rel="stylesheet" ng-repeat="(routeCtrl, cssUrl) in routeStyles" ng-href="{{cssUrl}}" />';
+ 				elem.append($compile(html)(scope));
+ 				scope.routeStyles = {};
+ 				// inject dependencies for state change
+ 				$rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+                    if(fromState && fromState.css){
+                        if(!Array.isArray(fromState.css)){
+                            fromState.css = [fromState.css];
+                        }
+                        angular.forEach(fromState.css, function(sheet){
+                            delete scope.routeStyles[sheet];
+                        });
+                    }
+                    if(toState && toState.css){
+                        if(!Array.isArray(toState.css)){
+                            toState.css = [toState.css];
+                        }
+                        angular.forEach(toState.css, function(sheet){
+                            scope.routeStyles[sheet] = sheet;
+                        });
+                    }
+                });
+ 			}
+ 		};
+ 	}
+ ]);
