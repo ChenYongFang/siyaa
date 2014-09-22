4d324036e096b5310208585c294896c97de6b60a
Jeremy <254330807@qq.com>
2014-09-22 11:59:25 +0800

    change lottery wheel

--- js/controllers/lotteryWheel.js
+++ js/controllers/lotteryWheel.js
@@ -20,98 +20,118 @@ define(['js/app'],function(app){
 
 	    var tween = null; //animate the start button obj
 	    var unluckAngle = []; //unluck angule
-	    var gifts = [{name:'美女一个',rate:1},{name:'Iphone5一台',rate:2},{name:'300元现金红包',rate:3},{name:'100淘积分',rate:4}];
+	    var gifts = [{name:'美女一个',rate:1},{name:'Iphone5一台',rate:2},{name:'300元现金红包',rate:3}];
 	    $scope.gifts = gifts;
 
 	 	var containerObj = document.getElementById('wrap-wheel');
-	 	var maxStageWidth = 770;
-		var maxStageHeight = 735;
+	 	var maxStageWidth = 800;
+		var maxStageHeight = 765;
 	    var maxStageXRadius = Math.round(maxStageWidth / 2);
 	    var maxStageYRadius = Math.round(maxStageHeight / 2);
 
-	 	var wheelBg = new Image();
-	 	wheelBg.onload = function(){
-	 		resizeStage(containerObj);
-	 		var image = new Kinetic.Image({
-	 			width:maxStageWidth,
-	 			height:maxStageHeight,
-	 			image:wheelBg
-	 		});
-	 		bgLayer.add(image);
+        // draw wheel background image.
+        loadImage('/images/lottery/wheel-bg.jpg',function(rawImage){
+            var image = new Kinetic.Image({
+                image:rawImage,
+                width:maxStageWidth,
+                height:maxStageHeight
+            });
+            bgLayer.add(image);
             stage.add(bgLayer);
-	        //draw prize shape.
+            //draw prize shape.
             drawPrizeShape(gifts);
-	 	};
-	 	wheelBg.src = '/images/lottery/wheel-bg.jpg';
+        });
 	    // draw arc prize shape
 	    function drawPrizeShape(gifts){
 	        var prizeCount = evalPrizeCount(gifts); // mixed prizes count.
 	        var shapeAngle = 360 / prizeCount; // each arc shape size.
-	        var startAngle = 90 % shapeAngle + shapeAngle / 2; //start wheel button angle
+	        var startAngle = 90 - (90 % shapeAngle); //start wheel button angle
 	        var prizes = mixPrizes(gifts);
-            var prizeShapeGroup = new Kinetic.Group();
+            var centraPoint = {
+                x:maxStageXRadius - 1,
+                y:maxStageYRadius + 62
+            }
+            var prizeShapeGroup = new Kinetic.Group({
+                x:centraPoint.x,
+                y:centraPoint.y,
+                offset:{x:centraPoint.x ,y:centraPoint.y}
+            });
 
-	        //draw start button image
-	        var startImg = new Image();
-	        startImg.onload = function(){
-	            var image = new Kinetic.Image({
-	                x:maxStageXRadius + 4,
-	                y:maxStageYRadius + 60,
-	                width:132,
-	                height:167,
-	                image:startImg,
-	                listening:true,
-	                rotation:startAngle,
-	                offset: {x:66, y:102}
-	            });
-                wheelLayer.add(image);
-                stage.add(wheelLayer);
-	            /* wheel the start button */
-	            image.on('tap click',function(){
+            //draw wrap prize circle image
+            loadImage('/images/lottery/wrap-circle.png',function(rawImage){
+                var image = new Kinetic.Image({
+                    x:82,
+                    y:128,
+                    width:634,
+                    height:634,
+                    image:rawImage
+                });
+                prizeShapeGroup.add(image);
+                wheelLayer.add(prizeShapeGroup);
+                drawShape(prizes,LOTTERY.COLORS);
+                resizeStage(containerObj);
+                //draw start button image
+                loadImage('/images/lottery/wheel-start.png',function(rawImage){
+                    var image = new Kinetic.Image({
+                        x:centraPoint.x,
+                        y:centraPoint.y,
+                        width:132,
+                        height:167,
+                        image:rawImage,
+                        listening:true,
+                        //rotation:startAngle,
+                        offset: {x:66, y:102}
+                    });
+                    wheelLayer.add(image);
+                    stage.add(wheelLayer);
+                    var isWheeling = false;
+                    /* wheel the start button */
+                    image.on('tap click',function(){
+                        if(isWheeling){
+                            return false;
+                        }
+                        var angle = unluckAngle[GetRandomNum(0,unluckAngle.length)] + 360 * 4;
+                        if(tween){
+                            tween.reset();
+                            prizeShapeGroup.draw();
+                        }
+                        tween = new Kinetic.Tween({
+                            node:prizeShapeGroup,
+                            duration: 4,
+                            rotation:angle,
+                            onFinish:function(){
+                                ModalService.open({
+                                    backdrop:'static',
+                                    windowClass:'wheel-result modal-vm',
+                                    templateUrl:'lottery-result.html',
+                                    controller:['$scope','$modalInstance',function($scope,$modalInstance){
+                                        $scope.msg = '很遗憾，您没有中奖在接再励哦！';
+                                        $scope.close = function(){
+                                            $modalInstance.close();
+                                            isWheeling = false;
+                                        }
+                                    }]
+                                });
+                            },
+                            easing: Kinetic.Easings.EaseOut
+                        });
+                        isWheeling = true;
+                        tween.play();
+                    });
+                });
+            });
 
-	            	var angle = unluckAngle[GetRandomNum(0,unluckAngle.length)] + 360 * 4;
-	            	
-	            	if(tween){
-	            		tween.reset();
-	            	}
-	            	tween = new Kinetic.Tween({
-	            		node:image,
-	            		duration: 3,
-	            		rotation:angle,
-	            		onFinish:function(){
-	            			ModalService.open({
-	            				backdrop:'static',
-	            				windowClass:'wheel-result modal-vm',
-	            				templateUrl:'lottery-result.html',
-	            				controller:['$scope','$modalInstance',function($scope,$modalInstance){
-	            					$scope.msg = '很遗憾，您没有中奖在接再励哦！';
-	            					$scope.close = $modalInstance.close;
-	            				}]
-	            			});
-	            		},
-	            		easing: Kinetic.Easings.StrongEaseOut
-	            	});
-	            	
-	            	console.info(tween.play());
-	            });
-	        };
-	        startImg.src = '/images/lottery/wheel-start.png';
-	        
-	        drawShape(prizes,LOTTERY.COLORS);
 	        // draw prize shape to circle
 	        function drawShape(prizes,colors){
-
-	            var x = maxStageXRadius + 6;
-	            var y = maxStageYRadius + 58;
-	            var innerRadius = 238;
+	            var innerRadius = 250;
 	            var shapeRadian = Math.PI / (prizeCount / 2);
-	            var textRadius = x / 2 - 18;
+	            var textRadius = centraPoint.x / 2 - 18;
 
 	            for(var i=0;i<prizes.length;i++){
 	                var radian = shapeRadian * i;
 	                var arc = new Kinetic.Arc({
-	                    x: x,
-	                    y: y,
+	                    x: centraPoint.x,
+	                    y: centraPoint.y,
 	                    angle: shapeAngle,
 	                    fill: colors[i],
 	                    innerRadius: innerRadius,
@@ -123,32 +143,27 @@ define(['js/app'],function(app){
 
 	                var textObj = prizes[i]; // draw text object
 	                var drawCtx = [];
+                    //Kinetic text config object.
+                    var textConfigObj = {
+                        fill:'#fff',
+                        fontSize: 40,
+                        fontStyle:'bold',
+                        fontFamily: 'sans-serif',
+                        x: centraPoint.x + Math.cos(funRadian) * textRadius,
+                        y: centraPoint.y + Math.sin(funRadian) * textRadius,
+                        rotation:(funRadian + Math.PI / 2) / Math.PI * 180
+                    };
 
 	                if(angular.isObject(textObj)){
-	                	var text = new Kinetic.Text({
-		                    fill:'#fff',
-		                    fontSize: 60,
-		                    fontStyle:'bold',
-		                    text: textObj.rate,
-		                    fontFamily: 'sans-serif',
-		                    x: x + Math.cos(funRadian) * textRadius,
-		                    y: y + Math.sin(funRadian) * textRadius,
-		                    rotation:(funRadian + Math.PI / 2) / Math.PI * 180
-		                });
-		                text.offsetX(text.width()/2);
+	                	var text = new Kinetic.Text(textConfigObj);
+                        text.text(textObj.rate);
+                        text.fontSize(60);
+		                text.offsetX(text.width() / 2);
                         prizeShapeGroup.add(text);
 		                text.offsetY(text.height());
-		                var text = new Kinetic.Text({
-		                    fill:'#fff',
-		                    fontSize: 40,
-		                    fontStyle:'bold',
-		                    text: '等奖',
-		                    fontFamily: 'sans-serif',
-		                    x: x + Math.cos(funRadian) * textRadius,
-		                    y: y + Math.sin(funRadian) * textRadius,
-		                    rotation:(funRadian + Math.PI / 2) / Math.PI * 180
-		                });
-		                text.offsetX(text.width()/2);
+		                var text = new Kinetic.Text(textConfigObj);
+                        text.text('等奖');
+		                text.offsetX(text.width() / 2);
                         prizeShapeGroup.add(text);
 	                }else{
 	                	//draw virtual prizes text
@@ -158,17 +173,9 @@ define(['js/app'],function(app){
 	                	}
 
 		                for(var j=0;j<drawCtx.length;j++){
-		                	var text = new Kinetic.Text({
-			                    fill:'#fff',
-			                    fontSize: 40,
-			                    fontStyle:'bold',
-			                    text: drawCtx[j],
-			                    fontFamily: 'sans-serif',
-			                    x: x + Math.cos(funRadian) * textRadius,
-			                    y: y + Math.sin(funRadian) * textRadius,
-			                    rotation:(funRadian + Math.PI / 2) / Math.PI * 180
-			                });
-			                text.offsetX(text.width()/2);
+		                	var text = new Kinetic.Text(textConfigObj);
+                            text.text(drawCtx[j]);
+			                text.offsetX(text.width() / 2);
 			                if(j%2 === 0){
 			                	text.offsetY(text.height());
 			                }
@@ -176,59 +183,51 @@ define(['js/app'],function(app){
 		                }
 	                }
 	            }
-                wheelLayer.add(prizeShapeGroup);
+
 	        }
 
 	        function evalPrizeCount(gifts){
 	            /* calculate prize count */
-	            if(gifts.length < 4){
-	                return 7;
-	            }else if(gifts.length < 6){
-	                return 8;
+	            if(gifts.length < 3){
+	                return 4;
 	            }else{
-	                return 9; // max supported gift count to be display.
+	                return 8;
 	            }
 	        }
 
 	        // mix the gifts and virtual prizes
 	        function mixPrizes(gifts){
+                var halfShapeAngle = shapeAngle / 2;
+                if(gifts.length > 8)
+                    gifts.length = 8;
 	            var prizes = new Array();
-	            var virtualPrizes = LOTTERY.VIRTUALPRIZES;
-	            var distance = Math.floor((prizeCount - gifts.length) / gifts.length);
+                var virtualPrizes = getRandomVirtualPrizes(prizeCount - gifts.length);
+                var vIndex = 0;
 
-	            for(var i=0;i<gifts.length;i++){
-	                //after add virtual prizes jump to second prize
-	                jumpIndex = i * distance + i;
-	                prizes[jumpIndex] = gifts[i];
-	                prizes[jumpIndex].angle = shapeAngle * (i + 2) + Math.floor(shapeAngle / 2);
-
-	                if(distance !== 0){
-	                    var tmpPrizes = getRandomVirtualPrizes(distance);
-	                    for(var j=0;j<tmpPrizes.length;j++){
-	                        prizes[++jumpIndex] = tmpPrizes[j];
-	                        //generate the unluck angle
-	                        unluckAngle.push(shapeAngle * (jumpIndex + 2) + startAngle);
-	                    }
-	                }
-	            }
-	            function getRandomVirtualPrizes(size){
-	                var tmpPrizes = new Array(size);
-	                for(var i=0;i<tmpPrizes.length;i++){
-	                    tmpPrizes[i] = virtualPrizes[GetRandomNum(0,virtualPrizes.length - 1)];
-	                }
-	                return tmpPrizes;
-	            }
-	            /* add random virtual prize to short prizes  */
-	            var shortPrizeCount = prizeCount - prizes.length;
-	            var shortIndex = prizes.length;
-	            for(var i=0;i<shortPrizeCount;i++){
-	                prizes[shortIndex + i] = virtualPrizes[GetRandomNum(0,virtualPrizes.length - 1)];
-	                //generate the short prize unluck angle
-	                unluckAngle.push(shapeAngle * (shortIndex + i + 2) + startAngle);
-	            }
-
-	            console.debug('prizes',prizes);
-	            console.debug('unluck angle',unluckAngle);
+                for(var i= 0;vIndex<gifts.length;i++,vIndex++){
+                    prizes[i] = gifts[vIndex];
+                    prizes[i].angle = (prizeCount - prizes.length) * shapeAngle + halfShapeAngle - startAngle;
+                    if(vIndex < virtualPrizes.length){
+                        prizes[++i] = virtualPrizes[vIndex];
+                        unluckAngle.push((prizeCount - prizes.length) * shapeAngle + halfShapeAngle - startAngle);
+                    }
+                }
+                /* add random virtual prize to short prizes  */
+                var left = virtualPrizes.length - vIndex;
+                for(var i=0;i<left;i++){
+                    prizes.push(virtualPrizes[i + vIndex]);
+                    unluckAngle.push((prizeCount - prizes.length) * shapeAngle + halfShapeAngle - startAngle);
+                }
+                console.debug('prizes',prizes);
+                console.debug('unluck angle',unluckAngle);
+                function getRandomVirtualPrizes(size){
+                    var virtualPrizes = LOTTERY.VIRTUALPRIZES;
+                    var tmpPrizes = new Array(size);
+                    for(var i=0;i<tmpPrizes.length;i++){
+                        tmpPrizes[i] = virtualPrizes[GetRandomNum(0,virtualPrizes.length - 1)];
+                    }
+                    return tmpPrizes;
+                }
 	            return prizes;
 	        }
 	        return unluckAngle;
@@ -237,7 +236,6 @@ define(['js/app'],function(app){
 	 	// Sets scale and dimensions of stage in relation to window size
 	 	function resizeStage(containerObj){
 	 		var scalePercentage = angular.element(containerObj).width() / maxStageWidth;
-
 	 		stage.setAttr('scaleX', scalePercentage);
 	    	stage.setAttr('scaleY', scalePercentage);
 	    	stage.setAttr('width', maxStageWidth * scalePercentage);
@@ -249,5 +247,14 @@ define(['js/app'],function(app){
 		window.addEventListener('resize', function(){
 			resizeStage(containerObj);
 		});
+
+        //load image
+        function loadImage(src,callback){
+            var image = new Image();
+            image.onload = function(){
+                (callback || angular.noop)(image);
+            }
+            image.src = src;
+        }
 	}]);
 });
